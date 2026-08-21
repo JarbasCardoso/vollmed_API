f87acfeef2242b507c4ccb14b085ae3086e06426
Jarbas Cardoso <jarbascardosolealfilho@yahoo.com.br>
2026-08-21 11:36:46 -0300

    Enviando versão final do dia 21 08 2926

--- src/main/java/br/com/sistema/api/controller/OlaController.java
+++ src/main/java/br/com/sistema/api/controller/OlaController.java
@@ -1,5 +1,17 @@
 package br.com.sistema.api.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController //Registra a classe OlaController como um controlador Rest do SpringBoot
+@RequestMapping("ola") //Define a url base para todos os metodos dessa classe
 public class OlaController {
-    
+    @GetMapping("/olamundo") //Método HTTP do tipo GET
+    public String olaMundo(){
+        return "Olá, Mundo!!!";
+    }
 }
+
+
+
--- src/main/java/br/com/sistema/api/controller/PacienteController.java
+++ src/main/java/br/com/sistema/api/controller/PacienteController.java
@@ -1,5 +1,22 @@
 package br.com.sistema.api.controller;
 
-public class PacienteController {
-    
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("paciente") //Define a url base para todos os metodos dessa classe
+public class PacienteController { 
+     //GET  Request -> Response -> Exemplo: localhost:8080/paciente/1
+
+     @GetMapping("/") // Aponta para localhost:8080/paciente
+     public String exibirHome() {
+             return "a";
+     };
+     //GET/POST Request -> Response -> Exemplo: Cadastrar paciente .exibea tela de cadstro e o POST é chamado quando enviar é clicado
+      
+     //GET/PUT Request -> Response -> Exemplo: Alterar telefone . Get exibe a tela de alteração e o PUT é chamado quando o botão alterar é clicado
+     //DELETE
+     //CRUD 
+
 }
